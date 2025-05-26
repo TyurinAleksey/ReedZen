6e29c471134817ac908dbd6a733403a41fe0ea6c
Aleksey Tyurin <alaxche@gmail.com>
2025-05-26 23:13:37 +1000

    update validations

--- DB/validations/auth.ts
+++ DB/validations/auth.ts
@@ -1,18 +1,18 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 export const registerSchema = z.object({
-  email: z.string().email('Некорректный email'),
-  password: z.string().min(6, 'Пароль должен быть минимум 6 символов'),
-  username: z.string().min(3, 'Имя пользователя должно быть минимум 3 символа'),
-  avatarUrl: z.string().url('Некорректный URL аватара').optional(),
-  role: z.enum(['user', 'admin']).default('user')
+  email: z.string().email("Некорректный email"),
+  password: z.string().min(6, "Пароль должен быть минимум 6 символов"),
+  username: z.string().min(3, "Имя пользователя должно быть минимум 3 символа"),
+  avatarUrl: z.string().url("Некорректный URL аватара").optional(),
+  role: z.enum(["user", "admin"]).default("user"),
 });
 
 export type RegisterInput = z.infer<typeof registerSchema>;
 
 export const loginSchema = z.object({
-    email: z.string().email('Некорректный email'),
-    password: z.string().min(6, 'Пароль должен содержать минимум 6 символов')
-  });
-  
-export type LoginInput = z.infer<typeof registerSchema>;
+  email: z.string().email("Некорректный email"),
+  password: z.string().min(6, "Пароль должен содержать минимум 6 символов"),
+});
+
+export type LoginInput = z.infer<typeof loginSchema>;
